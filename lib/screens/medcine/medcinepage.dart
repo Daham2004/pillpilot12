6f97837139099242cbefeaa63d76d566c4959ad7
Mifrah <125203561+m1frah@users.noreply.github.com>
2024-04-06 10:01:25 +0530

    Add Medicine and medicine details page

---
+++ lib/screens/medcine/medcinepage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import '../../widgets/medcinelist.dart';
+import 'addmeds.dart';
+class MedicationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Medication List'),
+      ),
+      body: MedicationListWidget(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+   
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddMedicinePage()),
+          );
+        },
+        tooltip: 'Add Medicine',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
